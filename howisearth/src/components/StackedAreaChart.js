3f50cdcf9419661ce974c1117abe9092c22e1451
JDygard <jdygard@gmail.com>
2022-06-03 11:41:59 +0200

    add basic node functionality

---
+++ howisearth/src/components/StackedAreaChart.js
@@ -0,0 +1,19 @@
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+// European countries
+// Request: "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/"
+// Query: "en/nama_10_gdp?geo=EU28&precision=1&na_item=B1GQ&unit=CP_MEUR&time=2010&time=2011"
+// USA/States: EIA website, go figure that shit out :P
+const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 200, pv: 2400, amt: 1100 }];
+
+const RenderLineChart = props => {
+    return (
+        <LineChart width={600} height={300} data={data}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+        </LineChart>
+    );
+}
+
+export default RenderLineChart;

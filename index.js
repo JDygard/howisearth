f4f733bd41d41afa504270aa3d3ebc56654cc534
JDygard <jdygard@gmail.com>
2022-06-14 11:36:14 +0200

    database functional

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 import express from 'express';
 import path from 'path';
+import { MongoClient } from 'mongodb';
 import { createServer } from "http";
 import { Server } from "socket.io";
+import 'dotenv/config';
 
 const app = express();
 
+
+// ============================= DB ==================================
+const uri = process.env.MONGODB;
+const mongo = new MongoClient(uri);
+
+async function run() {
+  try {
+    await mongo.connect();
+    const database = mongo.db('how_is_earth')
+    const earthDB = database.collection('earthDB')
+
+    const query = { test: 1 };
+    const movie = await earthDB.findOne(query)
+    console.log("============= DB CONNECTED ================")
+    console.log(movie)
+  } catch (error) {
+    console.log(error)
+  }
+    finally {
+    await mongo.close()
+    console.log("============= DB DISCONNECTED ================")
+  }
+}
+run().catch(console.dir)
+
+// =============================== END DB ==========================
 app.use(express.static('howisearth/build/'))
 
 const httpServer = createServer();
